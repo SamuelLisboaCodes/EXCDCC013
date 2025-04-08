3c461d66be5f19418cc7d71cb56b4ee6d69fd48f
Samuel Lisboa <157116443+SamuelLisboaCodes@users.noreply.github.com>
2025-04-08 08:51:23 -0300

    Update main.cpp

--- DCC013-Alunos-main/AVL/main.cpp
+++ DCC013-Alunos-main/AVL/main.cpp
@@ -1,122 +1,31 @@
 #include <iostream>
-#include <string>
 #include "AVL.h"
-
 using namespace std;
 
-int lerNumero()
-{
-	string s;
-	getline(cin, s);
-	int x = 0;
-	int n = s.length();
-	if (n == 0 || n > 6) // tamanho máximo arbitrário
-		return -100000; // a flag precisa estar fora do escopo de números aceitáveis
-	for (int i = ((s[0] == '-') ? 1 : 0); i < n; i++)
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-			x = x * 10 + s[i] - '0';
-		else
-			return -100000;
-	}
-	if (s[0] == '-')
-		x *= -1;
-	return x;
-}
+int main() {
+    AVL arvore;
 
-int filtrarEntrada()
-{
-	int n = lerNumero();
-	while (n == -100000)
-	{
-		cout << "Numero invalido. Tente novamente: ";
-		n = lerNumero();
-	}
-	return n;
-}
+    cout << "Inserindo elementos na árvore AVL...\n";
+    int valores[] = {30, 20, 40, 10, 25, 35, 50, 5, 15};
 
-int main()
-{
-	int a[] = {20, 13, 30, 11, 15, 25, 33, 10, 12, 14, 18};
-	AVL* arv = new AVL();
-	for (int i = 0; i < 11; i++)
-		arv->insere(a[i]);
-	int op = 0;
-	char str[10];
-	while (true)
-	{
-		cout << "\033[2J\033[1;1H";
-		arv->imprimeEmLargura();
-		cout << endl << "Selecione uma operacao:" << endl;
-		cout << " 1: Inserir no" << endl;
-		cout << " 2: Buscar valor" << endl;
-		cout << " 3: Remover no" << endl;
-		cout << " 4: Imprimir visualizacao" << endl;
-		cout << " 5: Imprimir ordem crescente" << endl;
-		cout << " 6: Destruir arvore" << endl;
-		cout << " 7: Encerrar o programa" << endl;
-		cout << endl << "Digite o numero correspondente: ";
-		op = lerNumero();
-		while (op < 1 || op > 7)
-		{
-			cout << "Operacao invalida. Tente novamente: ";
-			op = lerNumero();
-		}
-		switch (op)
-		{
-			case 1:
-				cout << "Digite o valor do no (numero inteiro): ";
-				arv->insere(filtrarEntrada());
-				cout << "Insercao realizada com sucesso. Pressione ENTER para continuar.";
-				lerNumero();
-				break;
+    for (int v : valores) {
+        arvore.inserir(v);
+        cout << "Inserido: " << v << endl;
+        arvore.imprimir();
+        cout << "------------------\n";
+    }
 
-			case 2:
-				cout << "Digite o valor a ser buscado (numero inteiro): ";
-				op = filtrarEntrada();
-				cout << "O numero " << op << ((arv->busca(op) == NULL) ? " NAO " : " ") << "existe na arvore. Pressione ENTER para continuar.";
-				lerNumero();
-				break;
+    cout << "\nRemovendo elementos...\n";
+    arvore.remover(40);
+    cout << "Removido: 40\n";
+    arvore.imprimir();
 
-			case 3:
-				cout << "Digite o valor do no a ser removido (numero inteiro): ";
-				op = filtrarEntrada();
-				if (arv->busca(op) == NULL)
-					cout << "O numero " << op << " NAO existe na arvore. Pressione ENTER para continuar.";
-				else
-				{
-					arv->remove(op);
-					cout << "No removido com sucesso. Pressione ENTER para continuar.";
-				}
-				lerNumero();
-				break;
+    arvore.remover(30);
+    cout << "Removido: 30\n";
+    arvore.imprimir();
 
-			case 4:
-				cout << endl;
-				arv->imprimeVisualizacao();
-				cout << endl << "Pressione ENTER para continuar.";
-				lerNumero();
-				break;
+    cout << "\nElementos em ordem crescente:\n";
+    arvore.imprimirOrdem();
 
-			case 5:
-				cout << endl;
-				arv->imprimeOrdemCrescente();
-				cout << endl << "Pressione ENTER para continuar.";
-				lerNumero();
-				break;
-
-			case 6:
-				cout << endl;
-				arv->destroiArvore();
-				cout << "Arvore destruida com sucesso. Pressione ENTER para continuar.";
-				lerNumero();
-				break;
-
-			default:
-				break;
-		}
-		if (op == 7)
-			break;
-	}
-	return 0;
+    return 0;
 }
